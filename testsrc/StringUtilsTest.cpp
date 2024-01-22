9c41ac38667bdf7bec81de57d586fb6f61070e84
jlfong <jlfong@COE-CS-pc2.OU.AD3.UCDAVIS.EDU>
2024-01-22 10:23:21 -0800

    Slice Testcases

--- testsrc/StringUtilsTest.cpp
+++ testsrc/StringUtilsTest.cpp
@@ -3,10 +3,37 @@
 
 TEST(StringUtilsTest, SliceTest){
     
-}
+    std::string inString = "Hello";
+
+    // Positive start and end
+    std::string resultOne = StringUtils::Slice(inString, 1, 4);
+    EXPECT_EQ(resultOne, "el");
+
+    // Negative start 
+    std::string resultTwo = StringUtils::Slice(inString, -1, 0);
+    EXPECT_EQ(resultTwo, "o");
+
+    // Negative end 
+    std::string resultThree = StringUtils::Slice(inString, 0, -1);
+    EXPECT_EQ(resultThree, "Hell");
+
+    // Negative start and end 
+    std::string resultFour = StringUtils::Slice(inString, -1,-2);
+    EXPECT_EQ(resultFour, "o"); 
+
+    // Equal index
+    std::string resultFive = StringUtils::Slice(inString, 2, 2);
+    EXPECT_EQ(resultFive, "");
+
+    // Index out of bounds
+    std::string resultSix = StringUtils::Slice(inString, 0, 7);
+    EXPECT_EQ(resultSix, "");
+
+};
 
 TEST(StringUtilsTest, Capitalize){
-    
+
+   
 }
 
 TEST(StringUtilsTest, Upper){
@@ -14,7 +41,8 @@ TEST(StringUtilsTest, Upper){
 }
 
 TEST(StringUtilsTest, Lower){
-    
+
+
 }
 
 TEST(StringUtilsTest, LStrip){
